5f3a16a70aa448e79e73cd28ae4e84bc92617e09
ligulfzhou <ligulfzhou53@gmail.com>
2022-11-27 12:18:06 +0800

    overview components

--- @types/xrplsniper.ts
+++ @types/xrplsniper.ts
@@ -1,3 +1,4 @@
+
 export interface LatestSummary {
     xrpl_latest_1h_cnt: number
     xrpl_latest_24h_cnt: number
@@ -8,6 +9,7 @@ export interface LatestSummary {
     xrpl_latest_7d_volumn: number
     xrpl_latest_30d_volumn: number
 }
+
 
 export interface LatestSummaryResponse {
     errcode: number
--- components/collection_page/Overview.tsx
+++ components/collection_page/Overview.tsx
@@ -1,6 +1,7 @@
 import styles from './Collection.module.css';
 import {LatestSummary} from "../../@types/xrplsniper";
-import {FC} from "react";
+import {FC, useEffect, useState} from "react";
+
 
 interface Props {
     loading: boolean
@@ -12,6 +13,35 @@ const Overview: FC<Props> = ({
                                  loading,
                                  summary
                              }) => {
+    const [items, setItems] = useState<StatItem[]>([])
+
+    useEffect(()=> {
+        if (!summary) {
+            return
+        }
+
+        const statItems: StatItem[] = [
+            {
+            'title': '1H',
+            'cnt': summary.xrpl_latest_1h_cnt,
+            'volumn': summary.xrpl_latest_1h_volumn
+            },{
+                'title': '24H',
+                'cnt': summary.xrpl_latest_24h_cnt,
+                'volumn': summary.xrpl_latest_24h_volumn
+            }, {
+                'title': '7D',
+                'cnt': summary.xrpl_latest_7d_cnt,
+                'volumn': summary.xrpl_latest_7d_volumn
+            }, {
+                'title': '30D',
+                'cnt': summary.xrpl_latest_30d_cnt,
+                'volumn': summary.xrpl_latest_30d_volumn
+            }
+        ]
+
+        setItems(statItems)
+    }, [summary, loading])
 
     return (
         <div className="overview-container">
@@ -25,156 +55,45 @@ const Overview: FC<Props> = ({
                 </p>
             </div>
 
-
             {/*loading没用起来。{loading}*/}
+            <div className="flex items-center justify-items-start gap-2 pb-3">
+                {loading&& (
+                    <> TODO: it is loading..</>
+                )}
 
-            <div className="flex items-center pb-3">
-                <div className={styles.panel}>
-                    <h3 className={styles.panel__title}>
-                        1H
-                    </h3>
-                    <div className="panel__content">
-                        <div className=''>
-                            <div>sales</div>
-                            <div>{summary?.xrpl_latest_1h_cnt}</div>
-                        </div>
-                        <div className=''>
-                           <div> volumn</div>
-                           <div>{summary?.xrpl_latest_1h_volumn} XRP</div>
-                        </div>
-
-                    </div>
-                </div>
-                <div className={styles.panel}>
-                    <h3 className={styles.panel__title}>
-                        24H
-                    </h3>
-                    <div className="panel__content">
-                        <div className=''>
-                            <div>sales</div>
-                            <div>{summary?.xrpl_latest_24h_cnt}</div>
-                        </div>
-                        <div className=''>
-                            <div> volumn</div>
-                            <div>{summary?.xrpl_latest_24h_volumn} XRP</div>
-                        </div>
-
-                    </div>
-                </div>
-                <div className={styles.panel}>
-                    <h3 className={styles.panel__title}>
-                        7D
-                    </h3>
-                    <div className="panel__content">
-                        <div className=''>
-                            <div>sales</div>
-                            <div>{summary?.xrpl_latest_7d_cnt}</div>
-                        </div>
-                        <div className=''>
-                            <div> volumn</div>
-                            <div>{summary?.xrpl_latest_7d_volumn} XRP</div>
-                        </div>
-
-                    </div>
-                </div>
-                <div className={styles.panel}>
-                    <h3 className={styles.panel__title}>
-                        30D
-                    </h3>
-                    <div className="panel__content">
-                        <div className=''>
-                            <div>sales</div>
-                            <div>{summary?.xrpl_latest_30d_cnt}</div>
-                        </div>
-                        <div className=''>
-                            <div> volumn</div>
-                            <div>{summary?.xrpl_latest_30d_volumn} XRP</div>
-                        </div>
-
-                    </div>
-                </div>
-
-
-            {/*    <div className={styles.panel}>*/}
-            {/*        <h3 className={styles.panel__title}>*/}
-            {/*            24H Volume*/}
-            {/*        </h3>*/}
-            {/*        <div className="panel__content">*/}
-            {/*            <p className="md:text-lg xl:text-2xl font-semibold">*/}
-            {/*<span className="flex flex-col"><span>*/}
-            {/*  1,311,744&nbsp;SOL*/}
-            {/*  <em className="*/}
-            {/*    list-bottom*/}
-            {/*    text-gray-600*/}
-            {/*    not-italic*/}
-            {/*    text-xs*/}
-            {/*    font-normal*/}
-            {/*    dark:text-white*/}
-            {/*  ">($19,033,408)</em>*/}
-            {/*</span> </span>*/}
-            {/*            </p>*/}
-            {/*            <p className="hidden md:block md:mt-4 uppercase text-xs text-gray-500">*/}
-            {/*                SF Collections:<br/>*/}
-            {/*                <strong>*/}
-            {/*  <span>*/}
-            {/*    <span>1,050,489&nbsp;SOL*/}
-            {/*    <em className="*/}
-            {/*      list-bottom*/}
-            {/*      text-gray-600*/}
-            {/*      not-italic*/}
-            {/*      text-xs*/}
-            {/*      font-normal*/}
-            {/*      dark:text-white*/}
-            {/*    ">($15,242,589)*/}
-            {/*    </em>*/}
-            {/*    </span>*/}
-            {/*  </span>*/}
-            {/*                </strong>*/}
-            {/*            </p>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*    <div className={styles.panel}>*/}
-            {/*        <h3 className={styles.panel__title}>*/}
-            {/*            7D Volume*/}
-            {/*        </h3>*/}
-            {/*        <div className="panel__content">*/}
-            {/*            <p className="md:text-lg xl:text-2xl font-semibold">*/}
-            {/*<span className="flex flex-col"><span>*/}
-            {/*  1,311,744&nbsp;SOL*/}
-            {/*  <em className="*/}
-            {/*    list-bottom*/}
-            {/*    text-gray-600*/}
-            {/*    not-italic*/}
-            {/*    text-xs*/}
-            {/*    font-normal*/}
-            {/*    dark:text-white*/}
-            {/*  ">($19,033,408)</em>*/}
-            {/*</span> </span>*/}
-            {/*            </p>*/}
-            {/*            <p className="hidden md:block md:mt-4 uppercase text-xs text-gray-500">*/}
-            {/*                SF Collections:<br/>*/}
-            {/*                <strong>*/}
-            {/*  <span>*/}
-            {/*    <span>1,050,489&nbsp;SOL*/}
-            {/*    <em className="*/}
-            {/*      list-bottom*/}
-            {/*      text-gray-600*/}
-            {/*      not-italic*/}
-            {/*      text-xs*/}
-            {/*      font-normal*/}
-            {/*      dark:text-white*/}
-            {/*    ">($15,242,589)*/}
-            {/*    </em>*/}
-            {/*    </span>*/}
-            {/*  </span>*/}
-            {/*                </strong>*/}
-            {/*            </p>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
+                {items.map(item=> (statItem(item)))}
             </div>
 
         </div>
     )
 }
+
+interface StatItem {
+    title: string
+    cnt: number
+    volumn: number
+}
+
+const statItem = (item: StatItem | null) => {
+
+    return (
+        <div className='panel p-2 hover:border-gray-900 w-48 bg-gray-900 rounded-xl'>
+            <h3 className={styles.panel__title}>
+                {item?.title}
+            </h3>
+            <div className="panel__content p-1">
+                <div className='flex items-center justify-between md:block'>
+                    <div className='text-th-fgd-3 md:pb-0.5 md:text-[0.65rem]'>sales</div>
+                    <div className='text-th-fgd-1 md:text-xs'>{item?.cnt}</div>
+                </div>
+                <div className='flex items-center justify-between md:block mt-2'>
+                    <div className='text-th-fgd-3 md:pb-0.5 md:text-[0.65rem]'> volumn</div>
+                    <div className='text-th-fgd-1 md:text-xs'>{item?.volumn} XRP</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 
 export default Overview;
